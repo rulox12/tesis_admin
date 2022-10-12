6fc49759c610bc857621c2e4160d9d97598e7de1
rulox12 <dbetancur@intergrupo.com>
2022-10-12 16:28:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Tener encuenta este comando para desplegar cualquier cambio en produccion
+ng deploy --base-href=/easy-pay-frontend/
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  email: 'danielpcpx@hotmail.com',
+  password: 'lomejor12',
+  // api: 'http://localhost:3000/api/',
+  // urlFrontend: 'http://localhost:4200/',
+  // api: 'https://easypayuco.herokuapp.com/api/',
+  urlFrontend: 'https://rulox12.github.io/easy-pay-frontend/',
+};
